f2812b05c22ba0610788efe1d724818cd6f1a097
Joe Burdick <josephdburdick@gmail.com>
2015-10-11 16:44:51 -0400

    Browse Happy -> for IE10 and below. Add identifiers to help support an overlay. Added window resize code but it's in ES2015 and there's no transpiler yet.

--- site/app/scripts/main.js
+++ site/app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!');
+/* jshint esnext: true */
+$(() => {
+  $('[data-toggle="overlay"]').on('click', (e) => {
+    e.preventDefault();
+    $('body').toggleClass('overlay-open');
+  });
+
+  $('window').on('resize', () => {
+    console.log(this.width());
+  }).trigger('resize');
+});
