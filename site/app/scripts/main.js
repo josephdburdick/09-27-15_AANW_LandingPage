e4522dfa24e05951aa85d83f5ed856b87861ee3f
Joe Burdick <josephdburdick@gmail.com>
2015-10-11 18:52:36 -0400

    Add more styles, more responsiveness, more overlay.

--- site/app/scripts/main.js
+++ site/app/scripts/main.js
@@ -1,11 +1,33 @@
 /* jshint esnext: true */
 $(() => {
+  'use strict';
+  let $body = $('body'),
+      $window = $(window),
+      overlayOpenClass = 'overlay-open';
+
   $('[data-toggle="overlay"]').on('click', (e) => {
     e.preventDefault();
-    $('body').toggleClass('overlay-open');
+    $body.toggleClass(overlayOpenClass);
+
+    if ($body.hasClass(overlayOpenClass)){
+      if (!$('#main-footer .vertical-container').length){
+        $('#main-footer .footer-content').wrap('<div class="vertical-container"><div class="vertical-content"></div></div>');
+      }
+    } else {
+      unwrapVerticalContainer();
+    }
   });
 
-  $('window').on('resize', () => {
-    console.log(this.width());
+  $window.on('resize', () => {
+    if ($(window).width() >= 768){
+      $body.removeClass(overlayOpenClass);
+      unwrapVerticalContainer();
+    }
   }).trigger('resize');
+
+  function unwrapVerticalContainer(){
+    if ($body.hasClass(overlayOpenClass)){
+      $('.vertical-container .vertical-content').unwrap();
+    }
+  }
 });
